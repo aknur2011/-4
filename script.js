01accace39d5ae39f8be3b7b50b2ecd0e43bd11e
aknur2011 <aknur8083@gmail.com>
2025-10-24 18:20:18 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let jyma = prompt('число нидели')
+switch (jyma) {
+    case ('1'):
+        alert('дуйшомбу')
+        break;
+    case ('2'):
+        alert('шейшенби')
+        break;
+    case ('3'):
+        alert('дуйшомбу')
+        break;
+    case ('4'):
+        alert('бейшемби')
+        break;
+    case ('5'):
+        alert('жума')
+        break;
+    case ('6'):
+        alert('ишемби')
+        break;
+    case ('7'):
+        alert('Джекшемби')
+        break;
+
+    default:
+        alert('jok andai')
+        break;
+}
